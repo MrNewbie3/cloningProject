e430b34e8601228ac37ceced699ed25d566c5b3b
Mr.Newbie <atharafiaffandy@gmail.com>
2022-06-16 10:09:00 +0700

    comitting into git

---
+++ src/mainPage/main.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+class Main extends React.Component {
+  state = {};
+  render() {
+    return (
+      <div className="App ml-5">
+        <div className="text">
+          <h1 className="text-2xl font-sans mt-6 ">Dashboard</h1>
+          <p className="text-xs">Terakhir diperbarui: 2022-06-14 06:36:58</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Main;
